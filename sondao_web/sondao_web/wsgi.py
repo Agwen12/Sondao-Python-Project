b6bb60ba7072760fcc86cb240e7f78c69d5356d4
PGorgolewski <pgorgolewski01@gmail.com>
2022-04-21 20:49:13 +0200

    setup

--- sondao_web/sondao_web/wsgi.py
+++ sondao_web/sondao_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for sondao_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sondao_web.settings')
 
 application = get_wsgi_application()

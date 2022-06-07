cb3ca32926df375631a2c8d4b376e3dc43fe850e
PGorgolewski <pgorgolewski01@gmail.com>
2022-06-08 00:14:37 +0200

    in forms choose up to 100 years ago

--- sondao/GUI/sondao_web/gentree/forms.py
+++ sondao/GUI/sondao_web/gentree/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Person, Relation, Document
+import datetime
+
 
 class PersonForm(forms.ModelForm):
     class Meta:
@@ -17,4 +19,9 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = '__all__'
-        widgets = {'date': forms.SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day"))}
+        widgets = {
+            'date': forms.SelectDateWidget(
+                empty_label=("Choose Year", "Choose Month", "Choose Day"),
+                years=range(datetime.date.today().year - 100, datetime.date.today().year + 1)
+            )
+        }

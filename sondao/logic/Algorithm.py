8bb3e1b79cbfa61b5206cf11b6e1ec889978f122
Pawel Pycia <pawel.pycia29@gmail.com>
2022-06-07 22:58:24 +0200

    small little change

--- sondao/logic/Algorithm.py
+++ sondao/logic/Algorithm.py
@@ -12,6 +12,7 @@ class Algorithm:
         self.relation_dict = relation_dict
         self.testator_id = testator_id
         self.testator = self.get_person_obj(testator_id)
+        self.inheritees = []
 
     def find_heir(self):
         """
@@ -19,10 +20,14 @@ class Algorithm:
         :return:
         """
 
-        print("CHILDREN", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",  self.testator.children)))
-        print("PARENTS", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",  self.testator.parents)))
-        print("SIBLINGS", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",  self.testator.siblings)))
-        print("SPOUSE", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",  self.testator.spouse)))
+        print("CHILDREN", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",
+                                   self.testator.children)))
+        print("PARENTS", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",
+                                  self.testator.parents)))
+        print("SIBLINGS", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",
+                                   self.testator.siblings)))
+        print("SPOUSE", list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}",
+                                 self.testator.spouse)))
 
         # print("INFO", dict(self.tree.nodes.data()))
         if len(self.testator.spouse) + len(self.testator.children) > 4:
@@ -30,6 +35,8 @@ class Algorithm:
             self.distibute_wealth(0.75, self.testator.children)
         else:
             self.distibute_wealth(1, self.testator.children, self.testator.spouse)
+
+        print(list(map(lambda x: f"{x.personal_info.name} {x.personal_info.surname}", self.inheritees)))
 
     def distibute_wealth(self, wealth: float, *args):
 
@@ -48,8 +55,9 @@ class Algorithm:
         for inheritee in inheritees:
             # print(inheritee.personal_info.name, inheritee.personal_info.surname)
             if inheritee.personal_info.can_inherit():
-                local_wealth -= equal_part
-                print(f"[INFO] {inheritee.personal_info.name} {inheritee.personal_info.surname} has inherited {equal_part} of money $$$$$")
+                # local_wealth -= equal_part
+                # print(f"[INFO] {inheritee.personal_info.name} {inheritee.personal_info.surname} has inherited {equal_part} of money $$$$$")
+                self.inheritees.append(inheritee)
                 self.set_color(inheritee, "green")
             else:
                 self.set_color(inheritee, "red")
